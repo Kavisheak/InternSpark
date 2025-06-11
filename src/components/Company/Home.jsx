e5152f720c976f14f6be93cbcb0e5d7e95e2d858
kavisheak <skavisheak.official@gmail.com>
2025-06-11 11:49:19 +0530

    Compeletd up to company starting page

---
+++ src/components/Company/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        {/* Title */}
+        <div className="relative flex flex-col items-center w-full">
+            <h1 className="text-6xl font-bold text-center text-white">
+                Hire Top Interns
+            </h1>
+            {/* For title shadow */}
+            <h1 className="-mt-1 text-6xl font-bold text-center text-white transform pointer-events-none opacity-20 blur-sm rotate-x-12">
+                Hire Top Interns
+            </h1>
+        </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Home
